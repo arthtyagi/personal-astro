a1c8ae8b62283ee81ba60e55bef3a494e57c9daf
Arth Tyagi <arthtyagi7@gmail.com>
2022-11-13 18:59:49 +0530

    update page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,10 @@ export function Link({
 }) {
   return (
     <a
-      className="hover:text-teal-500 dark:hover:text-teal-400 relative block px-3 py-2 transition"
+      className="hover:text-red-500 duration-500 relative block px-3 py-2 transition"
       href={href}
+      target="_blank"
+      rel="noopener noreferrer"
     >
       {children}
     </a>
@@ -18,16 +20,53 @@ export function Link({
 export function Navbar(): JSX.Element {
   return (
     <nav>
-      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+      <ul className="flex rounded-2xl px-5  py-1 border-[1px] border-neutral-800 bg-black text-sm font-medium text-white shadow-2xl shadow-white/10">
         <li>
-          <Link href="/">Home</Link>
+          <Link href="https://github.com/arthtyagi">
+            <svg
+              className="h-6 w-6 sm:h-8 sm:w-8"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Link>
         </li>
         <li>
-          <Link href="/about">About</Link>
+          <Link href="https://linkedin.com/in/arthtyagi">
+            <svg
+              className="h-6 w-8 sm:h-8 sm:w-8"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762
+                          0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966
+                          0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75
+                          1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586
+                          7-2.777 7 2.476v6.759z"
+              />
+            </svg>
+          </Link>
         </li>
 
         <li>
-          <Link href="/contact">Contact</Link>
+          <Link href="https://twitter.com/arthtyagi">
+            <svg
+              className="h-6 w-8 sm:h-8 sm:w-8"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.066 9.645c.183 4.04-2.83 8.544-8.164 8.544-1.622 0-3.131-.476-4.402-1.291 1.524.18 3.045-.244 4.252-1.189-1.256-.023-2.317-.854-2.684-1.995.451.086.895.061 1.298-.049-1.381-.278-2.335-1.522-2.304-2.853.388.215.83.344 1.301.359-1.279-.855-1.641-2.544-.889-3.835 1.416 1.738 3.533 2.881 5.92 3.001-.419-1.796.944-3.527 2.799-3.527.825 0 1.572.349 2.096.907.654-.128 1.27-.368 1.824-.697-.215.671-.67 1.233-1.263 1.589.581-.07 1.135-.224 1.649-.453-.384.578-.87 1.084-1.433 1.489z" />
+            </svg>
+          </Link>
         </li>
       </ul>
     </nav>
